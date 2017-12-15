d0af553c99ae30e51fe0abbddcc8b5476f78b5d5
Augusto Samame <augustosamame@gmail.com>
2017-12-15 10:49:01 -0500

    add js require for ckeditor 3

--- app/assets/javascripts/blog/application.js
+++ app/assets/javascripts/blog/application.js
@@ -12,7 +12,7 @@
 //
 //= require social-share-button
 //= require ckeditor/init
-//= require ckeditor/config
-//= require ckeditor/plugins/autogrow/plugin
-//= require ckeditor/plugins/notification/plugin
-//= require ckeditor/plugins/notification/lang/es
+//= require blog/ckeditor/config
+//= require blog/ckeditor/plugins/autogrow/plugin
+//= require blog/ckeditor/plugins/notification/plugin
+//= require blog/ckeditor/plugins/notification/lang/es
